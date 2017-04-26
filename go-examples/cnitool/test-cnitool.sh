56b075eba910d6e5e8eeaee7996e9a327e570b8f
Crazykev <crazykev@zju.edu.cn>
2017-04-26 16:21:40 +0800

    add cnitool go example
    Signed-off-by: Crazykev <crazykev@zju.edu.cn>

---
+++ go-examples/cnitool/test-cnitool.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -x
+set -o errexit
+set -o nounset
+set -o pipefail
+
+CNITOOL_ROOT=$(dirname "${BASH_SOURCE}")
+NETNS=test
+
+cleanup() {
+    sudo ip netns del $NETNS
+}
+
+trap "cleanup" EXIT SIGINT
+
+cd $CNITOOL_ROOT
+go build -o cnitool
+
+ifconfig
+sudo ip netns list
+
+sudo ip netns add $NETNS
+sudo CNI_PATH=/opt/cni/bin NETCONFPATH=/etc/cni/net.d ./cnitool add netlist /var/run/netns/$NETNS
+sudo CNI_PATH=/opt/cni/bin NETCONFPATH=/etc/cni/net.d ./cnitool del netlist /var/run/netns/$NETNS
